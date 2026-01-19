253c2051c204ac1b709e40a4dfe2ff96627fb684
Sachin Randive <r.sachin81@gmail.com>
2026-01-19 09:14:35 +0530

    First set up commit

---
+++ TikTokTutorialPro/App/TikTokTutorialProApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokTutorialProApp.swift
+//  TikTokTutorialPro
+//
+//  Created by Sachin Randive on 19/01/26.
+//
+
+import SwiftUI
+
+@main
+struct TikTokTutorialProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
