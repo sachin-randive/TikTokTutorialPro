69d09756032a26b81b8b49a970e3284b71673d79
Sachin Randive <r.sachin81@gmail.com>
2026-01-20 09:50:19 +0530

    User Interface - FeedView Cell UI setup.

--- TikTokTutorialPro/Core/Feed/FeedCell.swift
+++ TikTokTutorialPro/Core/Feed/FeedCell.swift
@@ -19,8 +19,75 @@ struct FeedCell: View {
                         .foregroundStyle(.white)
                 }
             VStack {
+                Spacer()
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Sachin Randive")
+                            .fontWeight(.semibold)
+                        Text("@sachinrandive preparing for SwiftUI")
+                    }
+                    .foregroundStyle(.white)
+                    .font(.subheadline)
+                    Spacer()
+                    VStack(spacing: 28) {
+                        Circle()
+                            .frame(width: 48, height: 48)
+                            .foregroundStyle(.gray)
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "heart.fill")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundStyle(.white)
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                                
+                        }
+                        Button {
+                            
+                        } label: {
+                            VStack {
+                                Image(systemName: "ellipsis.bubble.fill")
+                                    .resizable()
+                                    .frame(width: 30, height: 30)
+                                    .foregroundStyle(.white)
+                                Text("27")
+                                    .font(.caption)
+                                    .foregroundStyle(.white)
+                                    .bold()
+                            }
+                                
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bookmark.fill")
+                                .resizable()
+                                .frame(width: 24, height: 30)
+                                .foregroundStyle(.white)
+                                
+                        }
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrowshape.turn.up.right.fill")
+                                .resizable()
+                                .frame(width: 30, height: 30)
+                                .foregroundStyle(.white)
+                                
+                        }
+                    }
+                    
+                }
+                .padding(.bottom, 80)
                 
             }
+            .padding()
         }
     }
 }
