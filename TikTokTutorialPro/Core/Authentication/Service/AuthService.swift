65a8acabe9b88df1280f7f8823f3e6023af69165
Sachin Randive <r.sachin81@gmail.com>
2026-01-25 19:26:15 +0530

    Auth Service set up.

---
+++ TikTokTutorialPro/Core/Authentication/Service/AuthService.swift
@@ -0,0 +1,28 @@
+//
+//  AuthService.swift
+//  TikTokTutorialPro
+//
+//  Created by Sachin Randive on 25/01/26.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    
+    func login(withEmail email: String, password: String) async throws {
+        
+    }
+    
+    func register(withEmail email: String,
+                  password: String,
+                  username: String,
+                  fullname: String) async throws {
+        
+    }
+    
+    func signout() {
+        
+    }
+    
+}
---
+++ TikTokTutorialPro/Core/Authentication/ViewModel/LoginViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewModel.swift
+//  TikTokTutorialPro
+//
+//  Created by Sachin Randive on 25/01/26.
+//
+
+import Foundation
